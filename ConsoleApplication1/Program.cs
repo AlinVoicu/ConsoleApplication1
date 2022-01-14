f4b07f0356af3bc4b0ff499a434d070361a127ed
Student <Student@A25-7>
2022-01-14 14:42:40 +0200

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class EvaluareAritm
+    {
+        public static int Suma(int a,int b)
+        {
+            return a + b;
+        }
+    }
+    class Program
+    {
+        delegate int Evaluare(int x, int y);
+        static void Main(string[] args)
+        {
+            StreamReader sr = null;
+            int cnt = 0;
+            try
+            {
+                sr = new StreamReader("E:\\fisier_test.txt");
+                while (sr.ReadLine() != null)
+                    cnt++;
+            }
+            catch
+            {
+                Console.WriteLine("Upsy Dupsy Got A Error there :P");
+            }
+            finally
+            {
+                Console.WriteLine("Nr de linii:" + cnt);
+            }
+            Console.ReadKey();
+        }
+    }
+}
